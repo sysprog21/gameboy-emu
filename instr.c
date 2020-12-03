a25e8473ff299af3cd538e061256b41e8aaed7f0
Rsysz <33973536+Rsysz@users.noreply.github.com>
2020-12-04 01:09:05 +0800

    Create instr.c

---
+++ instr.c
@@ -0,0 +1,238 @@
+#include "cpu.h"
+#include "instr.h"
+
+void NOP(struct gb_s *gb, cpu_instr instr) {
+  
+}
+
+void LD16(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void LD(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void INC16(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void INC(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void DEC16(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void DEC(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void RLC(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void ADD16(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void ADD(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void RRC(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void STOP(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void RL(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void JR(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void RR(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void DAA(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void CPL(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void SCF(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void CCF(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void HALT(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void ADC(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void SUB(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void SBC(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void AND(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void XOR(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void OR(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void CP(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void RET(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void POP(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void JP(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void CALL(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void PUSH(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void RST(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void RETI(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void DI(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void EI(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void SLA(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void SRA(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void SWAP(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void SRL(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void BIT(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void RES(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void SET(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void JP_TARGET(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void JP_BWD(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void JP_FWD(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+void ERROR(struct gb_s *gb, cpu_instr instr) {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
