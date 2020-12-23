2bd3d095e8b4fc86d5622ed14b9e10ed539c1c6e
Rsysz <ymps9640508@gmail.com>
2020-12-23 18:48:18 +0800

    Move gb_get_save_size implementation to the dedicated C source

--- gameboy.c
+++ gameboy.c
@@ -7,3 +7,12 @@ void gb_run_frame(struct gb_s *gb)
     while (!gb->gb_frame)
         __gb_step_cpu(gb);
 }
+
+/* Gets the size of the save file required for the ROM. */
+uint_fast32_t gb_get_save_size(struct gb_s *gb)
+{
+    const uint_fast16_t ram_size_location = 0x0149;
+    const uint_fast32_t ram_sizes[] = {0x00, 0x800, 0x2000, 0x8000, 0x20000};
+    uint8_t ram_size = gb->gb_rom_read(gb, ram_size_location);
+    return ram_sizes[ram_size];
+}
--- gameboy.h
+++ gameboy.h
@@ -3255,13 +3255,7 @@ static void __gb_step_cpu(struct gb_s *gb)
 void gb_run_frame(struct gb_s *gb);
 
 /* Gets the size of the save file required for the ROM. */
-uint_fast32_t gb_get_save_size(struct gb_s *gb)
-{
-    const uint_fast16_t ram_size_location = 0x0149;
-    const uint_fast32_t ram_sizes[] = {0x00, 0x800, 0x2000, 0x8000, 0x20000};
-    uint8_t ram_size = gb->gb_rom_read(gb, ram_size_location);
-    return ram_sizes[ram_size];
-}
+uint_fast32_t gb_get_save_size(struct gb_s *gb);
 
 /* Set the function used to handle serial transfer in the front-end. This is
  * optional.
